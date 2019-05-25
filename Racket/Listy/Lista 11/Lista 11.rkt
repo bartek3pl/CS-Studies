17480f7fb77ffb0cb2f82f026d22364135274ebd
bartek3pl <bartekh1998@op.pl>
2019-05-25 13:03:14 +0200

    Dodano zad.6

--- Racket/Listy/Lista 11/Lista 11.rkt
+++ Racket/Listy/Lista 11/Lista 11.rkt
@@ -26,12 +26,24 @@
 ;(sublists '(1 2 3))
 
 ;;Zad.3
+;1)
 (define/contract (test1 x y)
   (parametric->/c [a b] (-> a b a))
   (if (not (eq? x y))
       x
-      0))
+      (- x 1)))
 
+;3) coś źle
+;(define/contract (test3 f1 f2)
+;  (parametric->/c [a b c] (-> (-> b c) (-> a b) (-> a c)))
+;  (if (and (f1 5)
+;           (eq? (f2 (cons 5 2)) 1))
+;      #t
+;      #f))
+  
+;(test3 (lambda (x) (number? x))
+;       (lambda (y) (car y)))
+  
 ;;Zad.5
 (define/contract (foldl-map f a xs)
   (parametric->/c [a b]
@@ -47,4 +59,46 @@
   (it a xs null))
 
 ;(foldl-map (lambda (x a) (cons a (+ a x))) 0 '(1 2 3 4))
- 
+
+;;Zad.6
+(define (expr/c e)
+  (flat-rec-contract expr
+                     (struct/c const number?)
+                     (struct/c op symbol? expr expr)
+                     (struct/c let-expr symbol? expr expr)
+                     (struct/c variable symbol?)))
+
+(struct const    (val)      #:transparent)
+(struct op       (symb l r) #:transparent)
+(struct let-expr (x e1 e2)  #:transparent)
+(struct variable (x)        #:transparent)
+
+(define/contract (subst x v e)
+  (-> symbol? expr/c expr/c expr/c)
+  (match e
+    [(op s l r)   (op s (subst x v l)
+                        (subst x v r))]
+    [(const n)    (const n)]
+    [(variable y) (if (eq? x y)
+                      (const v)
+                      (variable y))]
+    [(let-expr y e1 e2)
+     (if (eq? x y)
+         (let-expr y
+                   (subst x v e1)
+                   e2)
+         (let-expr y
+                   (subst x v e1)
+                   (subst x v e2)))]))
+
+(define/contract (eval e)
+  (-> expr/c (or/c number? string?))
+  (match e
+    [(const n)    n]
+    [(op '+ l r)  (+ (eval l) (eval r))]
+    [(op '* l r)  (* (eval l) (eval r))]
+    [(let-expr x e1 e2)
+     (eval (subst x (eval e1) e2))]
+    [(variable n) (error n "cannot reference an identifier before its definition ;)")]))
+
+;(eval (let-expr 'x (const 2) (op '+ (variable 'x) (const 1))))
