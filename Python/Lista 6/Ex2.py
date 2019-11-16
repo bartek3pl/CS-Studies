c7516469844d1fb97d0125efccef1bebcb5d04d5
bartek3pl <bartekh1998@op.pl>
2019-11-16 18:15:41 +0100

    Add class in List 6

--- Python/Lista 6/Ex2.py
+++ Python/Lista 6/Ex2.py
@@ -3,50 +3,52 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
-dict = {}
-prev_tags = []
 
+class HtmlWatcher():
 
-def get_HTML_from_website(url):
-    get_from_url = requests.get("http://" + url)
-    return get_from_url.text
+    def __init__(self):
+        self.dict = {}
+        self.prev_tags = []
 
+    def __get_HTML_from_website(self, url):
+        get_from_url = requests.get("http://" + url)
+        return get_from_url.text
 
-def check_HTML(html):
-    global prev_tags
-    html_tags = []
-    count = 0
+    def __check_HTML(self, html):
+        html_tags = []
+        count = 0
 
-    # print("---------WATCHING---------")
+        print("---------WATCHING---------")
 
-    for tag in html.prettify().split("\n"):
-        html_tags.append(str(tag).strip())
+        for tag in html.prettify().split("\n"):
+            html_tags.append(str(tag).strip())
 
-    if prev_tags == html_tags:
-        # print("no change")
+        if self.prev_tags == html_tags:
+            print("no change")
+            return ""
+
+        if html.title.string not in self.dict:
+            self.dict[html.title.string] = html_tags
+        else:
+            for line_index, (prev_tag, cur_tag) in enumerate(zip(self.prev_tags, html_tags)):
+                if prev_tag != cur_tag:
+                    count += 1
+                    print(
+                        f"{count} | Change from {prev_tag} to {cur_tag} in line {line_index}.")
+
+        self.prev_tags = html_tags
         return ""
 
-    if html.title.string not in dict:
-        dict[html.title.string] = html_tags
-    else:
-        for line_index, (prev_tag, cur_tag) in enumerate(zip(prev_tags, html_tags)):
-            if prev_tag != cur_tag:
-                count += 1
-                print(
-                    f"{count} | Change from {prev_tag} to {cur_tag} in line {line_index}.")
-
-    prev_tags = html_tags
-    return ""
+    def watch_HTML(self, url, wait_seconds):
+        while True:
+            html = self.__get_HTML_from_website(url)
+            soup = BeautifulSoup(html, 'html.parser')
+            self.__check_HTML(soup)
+            time.sleep(wait_seconds)
+        return ""
 
 
-def watch_HTML(url, wait_seconds):
-    while True:
-        html = get_HTML_from_website(url)
-        soup = BeautifulSoup(html, 'html.parser')
-        check_HTML(soup)
-        time.sleep(wait_seconds)
-    return ""
+html_watcher = HtmlWatcher()
 
-
-watch_HTML("bartek3pl.github.io/Github-Users-Finder/", 20)
-watch_HTML("geeksforgeeks.org/", 60)
+html_watcher.watch_HTML("bartek3pl.github.io/Github-Users-Finder/", 20)
+html_watcher.watch_HTML("geeksforgeeks.org/", 60)
